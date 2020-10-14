12756e8bcc9da6d2e3e879ac92b936cf7b06cf54
obashun22 <obashun22@yahoo.co.jp>
2020-10-15 01:37:07 +0900

    [add update] ホーム画面作成 通知Handlerの作成

--- Notish/ViewController.swift
+++ Notish/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var willNotice = true
+    
+    @IBOutlet weak var notificationHandler: UIButton!
+    @IBAction func tappedNotificationHandler(_ sender: Any) {
+        if willNotice {
+            willNotice = false
+            // 次回ここから
+            // notificationHandler.backgroundColor = UIColor
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        notificationHandler.layer.cornerRadius = 10
     }
 
-
+    
+    
 }
 
