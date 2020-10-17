af328f4fb3db1f4f96022de609777521bf03e984
obashun22 <obashun22@yahoo.co.jp>
2020-10-18 04:24:41 +0900

    [add update] 背景追加 仕様変更

--- Notish/ViewController.swift
+++ Notish/ViewController.swift
@@ -6,15 +6,6 @@
 //
 
 /*
-英単語の配列を定義
-カウントで先頭から見ていく
-通知オフでカウントリセット通知全削除
-通知オンで配列シャッフル0から数え上げ一定時間おき
-万一カウント一周しても0に初期化するようにした
-
-To Do
-単語が変わらない
-map関数でずっと先まで設定？
 */
 
 import UIKit
@@ -23,32 +14,60 @@ import UserNotifications
 class ViewController: UIViewController {
     
     private var willNotice = true
-    private var notificationCount = 0
+    private var willNoticeVocabulary: [String: String] = [
+        "apple": "リンゴ",
+        "banana": "バナナ",
+        "pantsu": "パンツ",
+        "tanaka": "田中",
+        "horita": "堀田",
+        "canada": "カナダ",
+    ]
+    private let center = UNUserNotificationCenter.current()
     
     @IBOutlet weak var notificationHandler: UIButton!
     @IBAction func tappedNotificationHandler(_ sender: Any) {
-        if willNotice { // 通知をしないように変更する
+        if willNotice {
+            // 通知をしないように変更
             willNotice = false
+            
             // 通知するボタンに変更
             notificationHandler.backgroundColor = UIColor.orange
             notificationHandler.setTitle("通知する", for: .normal)
+            center.removeAllPendingNotificationRequests()
+            print("通知予約を削除しました")
             
-            UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
-            notificationCount = 0
             print("通知をオフにしました")
-        } else { // 通知するように変更する処理
+        } else {
+            // 通知するように変更する処理
             willNotice = true
+            
             // 通知しないボタンに変更
             notificationHandler.backgroundColor = UIColor.gray
             notificationHandler.setTitle("通知しない", for: .normal)
             noticeVocabulary()
+            
             print("通知をオンにしました")
         }
     }
     
     @IBAction func tappedTestButton(_ sender: Any) {
         print("tappedTestButton")
-        noticeVocabulary()
+        let keys = [String](willNoticeVocabulary.keys).shuffled()
+        let word = keys[0]
+        let meaning = willNoticeVocabulary[word]!
+        let content = UNMutableNotificationContent()
+        content.title = "\(word)"
+        content.body = "\(meaning)"
+        content.sound = UNNotificationSound.default
+        
+        let identifier = NSUUID().uuidString
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
+        center.add(request) { (err) in
+            if let err = err {
+                print("通知の登録に失敗しました\(err)")
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -57,29 +76,34 @@ class ViewController: UIViewController {
         notificationHandler.layer.cornerRadius = 10
         notificationHandler.backgroundColor = UIColor.gray
         notificationHandler.setTitle("通知しない", for: .normal)
-        
+        noticeVocabulary()
     }
     
     private func noticeVocabulary() {
-        let content = UNMutableNotificationContent()
-        let identifier = NSUUID().uuidString
-        content.sound = UNNotificationSound.default
-        
-        vocabularyKeys = vocabularyKeys.shuffled()
-        if notificationCount >= vocabularyKeys.count { notificationCount = 0 }
-        content.title = "\(vocabularyKeys[notificationCount])"
-        content.body = "\(vocabulary[vocabularyKeys[notificationCount]] ?? "意味を取得できませんでした")"
-        
-        let timer = 60
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(timer), repeats: true)
-        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request) { (err) in
-            if let err = err {
-                print("タイマーをセットできませんでした。", err.localizedDescription)
-                return
+        let keys = [String](willNoticeVocabulary.keys).shuffled()
+        var count = 0
+        for hour in 6...23 {
+            for minute in [0, 30] {
+                if count >= keys.count { count = 0 }
+                let word = keys[count]
+                let meaning = willNoticeVocabulary[word]!
+                let content = UNMutableNotificationContent()
+                content.title = "\(word)"
+                content.body = "\(meaning)"
+                content.sound = UNNotificationSound.default
+                
+                let identifier = NSUUID().uuidString
+                let date = DateComponents(hour: hour, minute: minute)
+                let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
+                let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
+                center.add(request) { (err) in
+                    if let err = err {
+                        print("通知の登録に失敗しました\(err)")
+                    }
+                }
+                print("通知を登録しました\(hour)時\(minute)")
+                count += 1
             }
-            print("\(timer)秒後にタイマーをセットしました")
-            self.notificationCount += 1
         }
     }
 }
