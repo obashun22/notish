386e64138a3b8104dfe83fa7bf1798d15cf2a9fe
obashun22 <obashun22@yahoo.co.jp>
2020-10-20 18:06:14 +0900

    [add] ホーム画面作成

--- Notish/ViewController.swift
+++ Notish/ViewController.swift
@@ -13,98 +13,30 @@ import UserNotifications
 
 class ViewController: UIViewController {
     
-    private var willNotice = true
-    private var willNoticeVocabulary: [String: String] = [
-        "apple": "リンゴ",
-        "banana": "バナナ",
-        "pantsu": "パンツ",
-        "tanaka": "田中",
-        "horita": "堀田",
-        "canada": "カナダ",
-    ]
-    private let center = UNUserNotificationCenter.current()
-    
-    @IBOutlet weak var notificationHandler: UIButton!
-    @IBAction func tappedNotificationHandler(_ sender: Any) {
-        if willNotice {
-            // 通知をしないように変更
-            willNotice = false
-            
-            // 通知するボタンに変更
-            notificationHandler.backgroundColor = UIColor.orange
-            notificationHandler.setTitle("通知する", for: .normal)
-            center.removeAllPendingNotificationRequests()
-            print("通知予約を削除しました")
-            
-            print("通知をオフにしました")
-        } else {
-            // 通知するように変更する処理
-            willNotice = true
-            
-            // 通知しないボタンに変更
-            notificationHandler.backgroundColor = UIColor.gray
-            notificationHandler.setTitle("通知しない", for: .normal)
-            noticeVocabulary()
-            
-            print("通知をオンにしました")
-        }
+    @IBOutlet weak var bellImageView: UIImageView!
+    @IBOutlet weak var addButtonView: UIButton!
+    @IBOutlet weak var notificationToggleView: UIView!
+    @IBOutlet weak var notificationToggleSwitch: UISwitch!
+    @IBAction func tappedAddButton(_ sender: Any) {
     }
     
-    @IBAction func tappedTestButton(_ sender: Any) {
-        print("tappedTestButton")
-        let keys = [String](willNoticeVocabulary.keys).shuffled()
-        let word = keys[0]
-        let meaning = willNoticeVocabulary[word]!
-        let content = UNMutableNotificationContent()
-        content.title = "\(word)"
-        content.body = "\(meaning)"
-        content.sound = UNNotificationSound.default
-        
-        let identifier = NSUUID().uuidString
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
-        center.add(request) { (err) in
-            if let err = err {
-                print("通知の登録に失敗しました\(err)")
-            }
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        notificationHandler.layer.cornerRadius = 10
-        notificationHandler.backgroundColor = UIColor.gray
-        notificationHandler.setTitle("通知しない", for: .normal)
-        noticeVocabulary()
-    }
-    
-    private func noticeVocabulary() {
-        let keys = [String](willNoticeVocabulary.keys).shuffled()
-        var count = 0
-        for hour in 6...23 {
-            for minute in [0, 30] {
-                if count >= keys.count { count = 0 }
-                let word = keys[count]
-                let meaning = willNoticeVocabulary[word]!
-                let content = UNMutableNotificationContent()
-                content.title = "\(word)"
-                content.body = "\(meaning)"
-                content.sound = UNNotificationSound.default
-                
-                let identifier = NSUUID().uuidString
-                let date = DateComponents(hour: hour, minute: minute)
-                let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: true)
-                let request = UNNotificationRequest.init(identifier: identifier, content: content, trigger: trigger)
-                center.add(request) { (err) in
-                    if let err = err {
-                        print("通知の登録に失敗しました\(err)")
-                    }
-                }
-                print("通知を登録しました\(hour)時\(minute)")
-                count += 1
-            }
-        }
+        
+        // bellImageViewのセットアップ
+        let angle = 8.69 * CGFloat.pi / 180
+        let transRotate = CGAffineTransform(rotationAngle: CGFloat(angle))
+        bellImageView.transform = transRotate
+        
+        // addButtonViewのセットアップ
+        addButtonView.layer.cornerRadius = 18
+        
+        // notificationToggleViewのセットアップ
+        notificationToggleView.layer.cornerRadius = 18
+        
+        
     }
 }
 
